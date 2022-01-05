b0c982f230db493a5fb849eaecaafee45fad028d
habibshrabon <snhabib777@gmail.com>
2022-01-05 11:50:16 +0600

    some fixing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home/Home";
 import AuthProvider from "./context/AuthProvider/AuthProvider";
-import PrivateRoute from "./Pages/Login/PrivateRoute/PrivateRoute";
 import ProductDetails from "./ProductDetails/ProductDetails";
 import Login from "./Pages/Login/Login/Login";
 import Register from "./Pages/Login/Register/Register";
